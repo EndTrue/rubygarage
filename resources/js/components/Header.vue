823e2c17a053e3c5c6f75cfccfc332ae60d5c1be
EndTrue <bskiper@yandex.ru>
2019-04-19 10:48:55 +0300

    Little changes to routes and CSS

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-md navbar-light">
         <div class="container">
-            <router-link class="navbar-brand" to="/">Ruby Garage ToDo List</router-link>
+            <router-link class="navbar-brand" to="/tasks">Ruby Garage ToDo List</router-link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,20 +1,11 @@
-import Home from './components/Home.vue';
 import Login from './components/auth/Login.vue';
 import Signup from './components/auth/Signup.vue';
-import CustomersMain from './components/customers/Main.vue';
-import CustomersList from './components/customers/List.vue';
-import NewCustomer from './components/customers/New.vue';
-import Customer from './components/customers/View.vue';
 import Tasks from './components/tasks/View.vue';
 import Notfound from './components/Notfound.vue';
 
 export const routes = [
     {
-        path: '/',
-        component: Home,
-        meta: {
-            requiresAuth: true
-        }
+        path: '/', redirect: '/tasks'
     },
     {
         path: '/login',
